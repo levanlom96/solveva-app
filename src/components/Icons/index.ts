43585292363cd376a8cf2bf3a4c9e724375e37ba
Levan <levanlom96@gmail.com>
2025-08-11 13:56:52 +0400

    Add other location icons

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -2,5 +2,22 @@ import MagnifyingGlassIcon from './MagnifyingGlassIcon.tsx';
 import GlobalIcon from './GlobalIcon.tsx';
 import ExclamationMarkIcon from './ExclamationMarkIcon.tsx';
 import ExportJsonIcon from './ExportJsonIcon.tsx';
+import AirportIcon from './AirportIcon.tsx';
+import BbqIcon from './BbqIcon.tsx';
+import BeachIcon from './BeachIcon.tsx';
+import HotelIcon from './HotelIcon.tsx';
+import PortIcon from './PortIcon.tsx';
+import TrainIcon from './TrainIcon.tsx';
 
-export { MagnifyingGlassIcon, GlobalIcon, ExclamationMarkIcon, ExportJsonIcon };
+export {
+  MagnifyingGlassIcon,
+  GlobalIcon,
+  ExclamationMarkIcon,
+  ExportJsonIcon,
+  AirportIcon,
+  BbqIcon,
+  BeachIcon,
+  HotelIcon,
+  PortIcon,
+  TrainIcon,
+};
